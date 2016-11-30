8f49ffbb3039ce5b4738b5de267a55b8a1ff65db
Sorrel Brigman <sorrel.k@gmail.com>
2016-11-30 09:02:11 -0600

    wrote out more detailed solution plan

--- grades.js
+++ grades.js
@@ -1,7 +1,16 @@
 var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
 
 //Output how many of each grade
-
+	//create a counter for each grade;
+	//Sort through each number and grade
+	//IF the number is the grade, add to counter
+	//output the grades to the console
 //Output what is the lowest grade
+  //Sort through each number
+  //keep the number that is lowest
+  //output that number to the console
 
 //Output what is the highest grade
+  //Sort through each number
+  //keep the number that is highest
+  //output that number to the console
